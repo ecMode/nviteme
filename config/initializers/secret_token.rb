58a919fb1edbcb730b44d1034a1d5df01d4f2f64
ecGoing <echen.contact@gmail.com>
2013-10-23 21:01:51 -0400

    Adding rails skeleton and emberJs example still not sure how to route to static index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nviteme::Application.config.secret_key_base = '4449608325ca0f391f08fbdbebe0cb1815429b9135cf9d1c129474c4452e72361dc4891d2cfe4d0269cf13d4c2c98b52655863cbe9cfaa279561dcd1bf6963e1'
